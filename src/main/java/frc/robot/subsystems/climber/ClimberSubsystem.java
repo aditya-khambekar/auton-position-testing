d7fc264f1ac10c37dd32b48224c6df47b89da1bf
Michael <nengzhimj@gmail.com>
2025-01-05 09:48:16 -0600

    starting to implement drive

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems.climber;
 
+import java.util.Objects;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class ClimberSubsystem extends SubsystemBase {
-    // TODO
+    private static ClimberSubsystem instance;
+
+    public static ClimberSubsystem getInstance() {
+        return instance = Objects.requireNonNullElseGet(instance, ConcreteClimberSubsystem::new);
+    }
 }
