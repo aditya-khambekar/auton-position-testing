9a2e4959d6356eb5b5f268594e2d4b0845b1b73c
Michael Jiang <n.michael.jiang@gmail.com>
2025-01-09 21:42:30 -0600

    Added Choreo AutoRoutine example code need to add real paths and build autos

--- src/main/java/frc/robot/commands/AutoRoutines.java
+++ src/main/java/frc/robot/commands/AutoRoutines.java
@@ -11,14 +11,60 @@ public class AutoRoutines {
         this.factory = factory;
     }
 
-    public AutoRoutine followPathRoutine(String name) {
-        final AutoRoutine routine = factory.newRoutine(name);
-        final AutoTrajectory simplePath = routine.trajectory(name);
+    public AutoRoutine branching2024Auto() {
+        AutoRoutine routine = autoFactory.newRoutine("branching2024Auto");
 
+        // This routine uses segments between pre-defined handoff points.
+        // Expand the tooltip for information about their naming convention ->
+
+        // Load the routine's trajectories
+        AutoTrajectory startToC2 = routine.trajectory("startToC2");
+        AutoTrajectory C2toM1 = routine.trajectory("C2toM1");
+        AutoTrajectory M1toM2 = routine.trajectory("M1toM2");
+        AutoTrajectory M2toM3 = routine.trajectory("M2toM3");
+        AutoTrajectory M1toScore = routine.trajectory("M1toScore");
+        AutoTrajectory M2toScore = routine.trajectory("M2toScore");
+        AutoTrajectory M3toScore = routine.trajectory("M3toScore");
+        AutoTrajectory scoreToM2 = routine.trajectory("scoreToM2");
+        AutoTrajectory scoreToM3 = routine.trajectory("scoreToM3");
+
+        // When the routine starts, reset odometry, shoot the first gamepiece, then go to the "C2" location
         routine.active().onTrue(
-                simplePath.resetOdometry()
-                        .andThen(simplePath.cmd())
+                Commands.sequence(
+                        startToC2.resetOdometry(),
+                        shooterSubsystem.shoot(),
+                        startToC2.cmd()
+                )
         );
+
+        // Pick up and shoot the gamepiece at the "C2" location, then go to the "M1" location
+        startToC2.active().whileTrue(intakeSubsystem.intake());
+        startToC2.done().onTrue(shooterSubsystem.shoot().andThen(C2toM1.cmd()));
+
+        // Run the intake when we are approaching a gamepiece
+        routine.anyActive(C2toM1, scoreToM2, scoreToM3, M1toM2, M2toM3) //
+                .whileTrue(intakeSubsystem.intake());
+
+        // If we picked up the gamepiece, go score, then go to the next midline location
+        // If we didn't pick up the gamepiece, go directly to the next midline location
+
+        // M1
+        Trigger atM1 = C2toM1.done();
+        atM1.and(shooterSubsystem::noGamepiece).onTrue(M1toM2.cmd());
+        atM1.and(shooterSubsystem::hasGamepiece).onTrue(M1toScore.cmd());
+        M1toScore.done().onTrue(shooterSubsystem.shoot().andThen(scoreToM2.cmd()));
+
+        // M2
+        Trigger atM2 = routine.anyDone(scoreToM2, M1toM2); //
+        atM2.and(shooterSubsystem::noGamepiece).onTrue(M2toM3.cmd());
+        atM2.and(shooterSubsystem::hasGamepiece).onTrue(M2toScore.cmd());
+        M2toScore.done().onTrue(shooterSubsystem.shoot().andThen(scoreToM3.cmd()));
+
+        // M3
+        Trigger atM3 = routine.anyDone(scoreToM3, M2toM3);
+        atM3.and(shooterSubsystem::hasGamepiece).onTrue(M3toScore.cmd());
+        M3toScore.done().onTrue(shooterSubsystem.shoot());
+
         return routine;
     }
 }
